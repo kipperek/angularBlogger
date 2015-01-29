869a7d08db41bf5a5d3e2470b4dd9f46397d2c3b
Kacper Czechowicz <kacper.czechowicz@traffit.com>
2015-01-29 14:39:27 +0100

    INIT

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular
+  .module('myThingsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
