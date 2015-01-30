7c2790534da117b5f7d8ce1ad3e8c1baca478da8
Kacper Czechowicz <kacper.czechowicz@traffit.com>
2015-01-30 11:27:42 +0100

    tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,9 +23,10 @@ module.exports = function(config) {
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
-      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-touch/angular-touch.js',
       'app/scripts/**/*.js',
+      'app/modules/**/*.js',
       'test/mock/**/*.js',
       'test/spec/**/*.js'
     ],
@@ -62,12 +63,12 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_INFO
 
     // Uncomment the following lines if you are using grunt's server to run the tests
-    // proxies: {
-    //   '/': 'http://localhost:9000/'
-    // },
+    //proxies: {
+    //   '/': 'http://localhost:8000/'
+    //}
     // URL root prevent conflicts with the site root
     // urlRoot: '_karma_'
   });
